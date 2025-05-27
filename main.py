8d5462bf92bcfb1be3e8049e903ed681f22f9f7f
NH Prince Pradhan <nurulhudaprince18@gmail.com>
2025-05-27 17:28:05 +0600

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ import time
 import sys
 
 # === COLORS ===
-RED = "\033[31m"
-GREEN = "\033[32m"
-YELLOW = "\033[33m"
-BLUE = "\033[34m"
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+YELLOW = "\033[1;33m"
+BLUE = "\033[0;34m"
 MAGENTA = "\033[35m"
-CYAN = "\033[36m"
-WHITE = "\033[37m"
+CYAN = "\033[0;36m"
+WHITE="\033[1;37m"
 RESET = "\033[0m"
 
 
